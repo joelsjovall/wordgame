fa4d95eb230b810a1bd94aa6ad6c24c03d6028cc
joelsjovall <joel.sjovall@gmail.com>
2026-04-15 11:03:28 +0200

    add round bidding API

--- Server/Services/IRoundService.cs
+++ Server/Services/IRoundService.cs
@@ -4,5 +4,6 @@ namespace Server.Services;
 
 public interface IRoundService
 {
+    Task<RoundBidResult> PlaceBidAsync(int roundId, int playerId, int bidCount, CancellationToken cancellationToken = default);
     RoundBidResult PlaceBid(Round round, int playerId, int bidCount);
 }
